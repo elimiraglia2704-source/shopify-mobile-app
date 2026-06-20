5eca8af805f4d3148b125efe582563509ee64a68
Eliseo2704 <eliseomiraglia2704@gmail.com>
2026-06-20 20:00:09 +0200

    Fix next.config.ts by removing redirects

--- next.config.ts
+++ next.config.ts
@@ -15,15 +15,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/api/auth/install',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
